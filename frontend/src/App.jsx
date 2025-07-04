6d77f0f59f81efd02c78c4592988462003d0eb2c
ovofederio <94723028+ovofederio@users.noreply.github.com>
2025-07-04 01:25:36 -0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 
-const categories = ["politics", "sports", "entertainment", "technology","health","business", ];
+const categories = ["breaking-news", "politics", "sports", "entertainment", "technology","health","business", ];
 const emojiMap = {
+  "breaking-news": "💥",
   "politics": "🗳️",
   "sports": "🏀",
   "entertainment": "🎬",
